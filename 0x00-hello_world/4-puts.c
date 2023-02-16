e27a83b67671308ab373b11830a1902391b0bb1c
Omewah <joelomewah@ymail.com>
2023-02-16 11:04:09 -0800

    using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - A program that uses the put function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
