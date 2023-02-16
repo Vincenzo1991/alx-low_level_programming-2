9dfa337e117637b7d929b375887fefcf67e99fe0
Omewah <joelomewah@ymail.com>
2023-02-16 12:27:49 -0800

    print with unix input output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - using the write function to print a statement
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
