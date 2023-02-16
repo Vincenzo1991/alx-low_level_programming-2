5b2b070915ccf8351d0657ba6cc33b68a8d11fc8
Omewah <joelomewah@ymail.com>
2023-02-16 11:37:02 -0800

    various sizes on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the sizes of various types on the computer
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int l;
+	long long int p;
+	float f;
+
+	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(p));
+	printf("Size of a float: %lu.\n", (unsigned long)sizeof(f));
+	return (0);
+}
