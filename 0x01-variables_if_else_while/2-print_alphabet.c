9b979fd725e4468cde69e51ffbc6ad450302ab50
Omewah <joelomewah@ymail.com>
2023-02-17 13:21:26 -0800

    print lower case letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints letters of the alphabet in lower case
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char lower_case = 'a';
+
+	while
+		(lower_case <= 'z');
+
+	{
+		putchar(lower_case);
+		lower_case++;
+	}
+
+	putchar('\n');
+	return (0);
+}
