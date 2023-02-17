405ff5986d68a70b0df476e890043e5d8d1e6c67
Omewah <joelomewah@ymail.com>
2023-02-17 14:10:19 -0800

    print letters in lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char c[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(c[i]);
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
