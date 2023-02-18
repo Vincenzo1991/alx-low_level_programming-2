aafba16f239491bde85b8734fa0fb0784ba11d39
Omewah <joelomewah@ymail.com>
2023-02-17 17:17:41 -0800

    print the combination of two two numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,13 +19,14 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i != 98 || j != 99)
+			if (i != 99 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
