773ea59f72a240ad18b42ebb36ad63d3e4f60a51
Omewah <joelomewah@ymail.com>
2023-04-03 15:01:51 -0700

    fix code to add node to a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,13 +5,28 @@
  * @idx: index of the list where the new node should be added
  * @n: the integer of the new node
  * @head: the first node
- * Return: NULL, if cannot add a new node
+ * Return: NULL, if failed, else the address of the new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *cpy = *head;
-	unsigned int node;
+	listint_t *new, *tmp;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	if (idx != 0)
+	{
+		tmp = *head;
+		for (i = 0; i < (idx - 1) && tmp != NULL; i++)
+		{
+			tmp = tmp->next;
+		}
+
+		if (tmp == NULL)
+			return (NULL);
+	}
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
@@ -21,21 +36,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new->next = cpy;
+		new->next = *head;
 		*head = new;
 		return (new);
 	}
-
-	for (node = 0, node < (idx - 1); node++;)
-	{
-		if (cpy == NULL || cpy->next == NULL)
-			return (NULL);
-
-		cpy = cpy->next;
-	}
-
-	new->next = cpy->next;
-	cpy->next = new;
-
+	new->next = tmp->next;
+	tmp->next = new;
 	return (new);
 }
